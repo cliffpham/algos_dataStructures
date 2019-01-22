9c56bae8093e50bd5d2e183e997ee470db6ecb8c
cliffpham <cliffpham@gmail.com>
2019-01-22 15:21:02 -0800

    [backtracking] solved

--- algos/sudoku.py
+++ algos/sudoku.py
@@ -1,13 +1,27 @@
   # for each potential item to be placed check the row, col, sub-box, to ensure that the value does not exist, else try the next item
 import math
 
-def solve(b):
-    spaces_to_fill = search(b)
+def sudoku(b):
+    to_fill = search(b)
+    result = solve(b, to_fill, 0)
+    
+    return result
 
-    for space in spaces_to_fill:
-        if place_number(b, space):
-            continue
-    return b 
+def solve(b, to_fill, i):
+    if i == len(to_fill):
+        return b
+    
+    cur_space = to_fill[i]
+    options = get_options(b, cur_space)
+   
+    for option in options:
+        if can_place(b, cur_space, option):
+            place(b, cur_space, option)
+            if solve(b,to_fill,i+1):
+                return b
+            else:
+                undo(b, cur_space)
+    return False
 
 def search(b):
     q = []
@@ -17,29 +31,45 @@ def search(b):
                 q.append((row,space))
     return q
 
-def place_number(b, candidate):
-    x = candidate[0]
-    y = candidate[1]
+def get_options(b,space):
+    x = space[0]
+    y = space[1]
 
-    options = [1,2,3,4,5,6,7,8,9]
+    numbers = [1,2,3,4,5,6,7,8,9]
+    options = []
 
-    for choice in options:
-        if check_row(b,x,choice) and check_col(b,y,choice) and check_sub(b, x, y, choice):
-            b[x][y] = choice
-            return True
-        else:
-            print('hit, choice was: ', end='')
-            print(choice)
-            undo(b,x,y)
+    for num in numbers:
+        if check_row(b,x,num) and check_col(b,y,num) and check_sub(b, x, y, num):
+            options.append(num)
 
-def undo(b,x,y):
-    b[x][y] == 0
-    return
+    return options
+
+#check if current num can be placed in spot, if not return false
+
+def can_place(b, space, option):
+    x = space[0]
+    y = space[1]
+
+    if check_row(b,x,option) and check_col(b,y,option) and check_sub(b, x, y, option):
+        return True
+
+    return False
+
+def place(b, space, option):
+    x = space[0]
+    y = space[1]
+
+    b[x][y] = option
+
+def undo(b,space):
+    x = space[0]
+    y = space[1]
+    
+    b[x][y] = 0
 
 def check_row(b,x,n):
     for i in range(len(b)):
         if b[x][i] == n:
-            # print(b[x][i])
             return False
     return True
 
@@ -57,7 +87,7 @@ def check_sub(b,x,y,n):
  
     top_left_sub_row = region_size * i
     top_left_sub_col = region_size * j
-
+ 
     for k in range(3):
         for l in range(3):
             if b[top_left_sub_row + k][top_left_sub_col + l] == n:
@@ -65,33 +95,15 @@ def check_sub(b,x,y,n):
     return True
 
 board = [
- [0,2,0],
- [1,0,9],
- [4,5,0]]
+    [2,0,0,0,1,7,6,0,0],
+    [4,6,7,5,0,0,0,8,0],
+    [1,5,0,2,0,0,0,0,0],
+    [0,9,2,3,0,0,7,0,0],
+    [3,0,0,0,0,0,0,0,8],
+    [0,0,5,0,0,1,4,6,0],
+    [0,0,0,0,0,6,0,3,2],
+    [0,2,0,0,0,3,5,9,6],
+    [0,0,6,1,5,0,0,0,7]
+]
 
-board2= [
-  [4,0,0,0,0,0,8,0,5],
-  [0,3,0,0,0,0,0,0,0],
-  [0,0,0,7,0,0,0,0,0],
-  [0,2,0,0,0,0,0,6,0],
-  [0,0,0,0,8,0,4,0,0],
-  [0,0,0,0,1,0,0,0,0],
-  [0,0,0,6,0,3,0,7,0],
-  [5,0,0,2,0,0,0,0,0],
-  [1,0,4,0,0,0,0,0,0]
- ]
-
-board_result = [
-    [4, 1, 2, 3, 6, 9, 8, 0, 5], 
-    [6, 3, 5, 1, 2, 4, 7, 9, 0], 
-    [8, 9, 0, 7, 5, 0, 1, 2, 3], 
-    [3, 2, 1, 4, 7, 5, 9, 6, 8], 
-    [7, 5, 6, 9, 8, 2, 4, 1, 0], 
-    [9, 4, 8, 0, 1, 6, 2, 3, 7], 
-    [2, 8, 9, 6, 4, 3, 5, 7, 1], 
-    [5, 6, 3, 2, 9, 1, 0, 4, 0], 
-    [1, 7, 4, 5, 0, 8, 3, 0, 2]]
-
-
-# print(solve(board))
-print(solve(board2))
+print(sudoku(board))
