fd4dc33602c0ddd46c7192d90db68ad5f7b50968
cliffpham <cliffpham@gmail.com>
2019-02-04 16:47:16 -0800

    [strings] solution for leetcode

--- algos/strings/first_unique_char.py
+++ algos/strings/first_unique_char.py
@@ -23,35 +23,49 @@ class test_first_unique(unittest.TestCase):
             -1
         )
 
-    def test5_first_unique(self):
-        self.assertEqual(
-            first_unique('cc'),
-            -1
-        )
-
 def first_unique(s):
-    count = []
-    first_char = -1
+    seen = {}
+    min_val = float('inf')
 
-    for n in range(len(s)):
-       n = 0
-       count.append(n)
-
-    #loop through all characters
     for i in range(len(s)):
-        count[i] += 1
-        for j in range(i+1, len(s)):
-            if s[j] == s[i]:
-                count[i] += 1
-                count[j] += 1
+        if s[i] in seen:
+            seen[s[i]] = -1
+        else:
+            seen[s[i]] = i
 
-    #loop through count and find first index that equals 1
-    for i in range(len(count)):
-        if count[i] == 1:
-            first_char = i
-            break
+    for val in seen.values():
+        if val != -1:
+            min_val = min(val, min_val)
+    
+    if min_val == float('inf'):
+        return -1
+    
+    return min_val
 
-    return first_char
+#brute force
+# def first_unique(s):
+#     count = []
+#     first_char = -1
+
+#     for n in range(len(s)):
+#        n = 0
+#        count.append(n)
+
+#     #loop through all characters
+#     for i in range(len(s)):
+#         count[i] += 1
+#         for j in range(i+1, len(s)):
+#             if s[j] == s[i]:
+#                 count[i] += 1
+#                 count[j] += 1
+
+#     #loop through count and find first index that equals 1
+#     for i in range(len(count)):
+#         if count[i] == 1:
+#             first_char = i
+#             break
+
+#     return first_char
 
 if __name__ == "__main__":
     unittest.main()
