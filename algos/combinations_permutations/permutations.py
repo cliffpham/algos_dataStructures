487e94fabff542c1c30f9e9db9add2b15e619f25
cliffpham <cliffpham@gmail.com>
2019-02-07 11:27:15 -0800

    [permutations] leetcode solution

--- algos/combinations_permutations/permutations.py
+++ algos/combinations_permutations/permutations.py
@@ -1,15 +1,40 @@
-def perms(arr, i, cur, used):
-    for i in range(0, len(arr)):
-        if not i in used:
-            cur.append(arr[i])
-            used[i] = True
-            perms(arr, i, cur, used)
-            cur.pop()
-            del used[i]
-    print(cur)
+def recur(nums, i, cur, output, seen):
+    if len(cur) == len(nums):
+        output.append(cur[:])
+        return output
+    
+    if i >= len(nums):
+        return output
+    
+    recur(nums, i+1, cur, output, seen)
+    
+    if not i in seen:
+        cur.append(nums[i])
+        seen[i] = True
+        recur(nums, 0, cur, output, seen)
+        cur.pop()
+        del seen[i]
+    
+    return output
+
+
+def permute(nums):
+
+    return recur(nums, 0, [], [], {})
+
+
+# def perms(arr, i, cur, used):
+#     for i in range(0, len(arr)):
+#         if not i in used:
+#             cur.append(arr[i])
+#             used[i] = True
+#             perms(arr, i, cur, used)
+#             cur.pop()
+#             del used[i]
+#     print(cur)
    
 
-print(perms([1,2,3], 0, [], {}))
+# print(perms([1,2,3], 0, [], {}))
 
 
 # def all_perm(arr, i, cur, used):
@@ -33,3 +58,8 @@ print(perms([1,2,3], 0, [], {}))
 
     
 
+
+
+# print(permute([1,2,3]))
+
+# print(perms([1,2,3], 0, [], {}))
