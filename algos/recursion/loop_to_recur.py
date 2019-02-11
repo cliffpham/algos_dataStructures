4d37f47d9c5641144bb6d3cc68a3c9d0c2c38f64
cliffpham <cliffpham@gmail.com>
2019-02-10 23:32:55 -0800

    [review] revised errors in nested recur func

--- algos/recursion/loop_to_recur.py
+++ algos/recursion/loop_to_recur.py
@@ -16,24 +16,81 @@ def recur_add(n, i, acc):
 # for i = 100; i > 0; i--:
 #     acc *= i
 
-def recur_multiply(n, acc = 1):
-    if n < 1:
+def multiply(n):
+    acc = 1
+    for i in range(n, 1, -1):
+        acc *= i
+
+    return acc
+
+# print(multiply(5))
+
+def multiply_recur(n, acc=1):
+    if n == 0:
         return acc
+
     acc *= n
-    return recur_multiply(n-1, acc)
 
-# print(recur_multiply(100))
+    return multiply_recur(n-1, acc)
+    
 
-#nested_loops
+# print(multiply_recur(5))
+
+# def recur_multiply(n, acc = 1):
+#     if n < 0:
+#         return 0
+#     j = 0
+#     while j < n:
+#         print(acc, (n*j))
+#         acc += (n*j)
+#         j += 1
+        
+#     return acc + recur_multiply(n-1)
+
+# print(recur_multiply(5))
+
+# nested_loops
 # acc = 1
 # for i = 100; i > 0; i--:
 #     for j = 0; j < 100; j++:
 #       acc += (i * j)
-def recur_nested(i, j, acc=1):
-    if i < 0 and j > 100:
+
+def nested_loops(n):
+    acc = 1
+    for i in range(n, -1, -1):
+        for j in range(i):
+            print(acc, i, j)
+            acc += i * j
+        
+    return acc
+
+def recur_nested(n, j, acc=1):
+   
+    if n == 0:
         return acc
     
-    acc += (i * j)
+    if j == n:
+        return recur_nested(n-1, 0, acc)
     
-    return recur_nested(i-1, j+1, acc)
-# print(recur_nested(100,0))
+    acc += (n*j)
+    print(acc, n, j)
+    
+    return recur_nested(n, j+1, acc)
+
+# def recur_nested(n, i, j, acc=1):
+#     # print(i, j)
+
+#     if i < 0:
+#         return acc
+    
+#     acc += (i * j)
+
+#     if j < 5:
+#         return recur_nested(i, j+1, acc)
+    
+#     else:
+#         return recur_nested(i-1, 0, acc)
+    
+print(recur_nested(5,0))
+print('**************')
+print(nested_loops(5))
