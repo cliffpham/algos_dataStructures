ed29408d45961eb39949f2926849774445360061
cliffpham <cliffpham@gmail.com>
2019-02-13 11:40:26 -0800

    [dp] leetcode solution

--- algos/dp/target_sum.py
+++ algos/dp/target_sum.py
@@ -1,41 +1,54 @@
-#brute force n(factorial)
+import unittest
 
-from itertools import product
+class test(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(
+            target_sum([1, 1, 1, 1, 1], 3, 0, {}),
+            5
+        )
+    def test2(self):
+        self.assertEqual(
+            target_sum([10,6,4,2,5],5,0,{}),
+            2
+        )
 
-def combo(nums):
-    return [[num * mul for num, mul in zip(nums, combo)] for combo in product([1, -1], repeat=len(nums))]
-
-def target_sum(nums, S):
-    combos = combo(nums)
-    counter = 0
-
-    for i in combos:
-        if sum(i) == S:
-            counter += 1
+def target_sum(nums, target, i, cache):
+    ck = "%s:%s" % (target, i)
     
-    return counter
+    if ck in cache:
+        return cache[ck]
 
-def target_sums(nums, target, i):
-    
-    if target == 0:
-        return 1
-
-    if target < 0:
-        return 0
-    
     if i >= len(nums):
+        if target == 0:
+            return 1
         return 0
 
-    l = target_sums(nums, target, i+1)
-    r = target_sums(nums, target - nums[i], i+1)
+    l = target_sum(nums, target + nums[i], i+1, cache)
+    r = target_sum(nums, target - nums[i], i+1, cache)
+    cache[ck] = l+r
  
     return l+r
 
-# print(target_sum([1, 1, 1, 1, 1], 3))
-print(target_sum([1, 2, 3, 4, 5], 3))
-print(target_sum([10, 6, 4, 2, 5], 5))
+#brute force n(factorial)
 
-print(target_sums([1, 2, 3, 4, 5], 3, 0))
-print(target_sums([10, 6, 4, 2, 5], 5, 0))
+# from itertools import product
+
+# def combo(nums):
+#     return [[num * mul for num, mul in zip(nums, combo)] for combo in product([1, -1], repeat=len(nums))]
+
+# def target_sum(nums, S):
+#     combos = combo(nums)
+#     counter = 0
+
+#     for i in combos:
+#         if sum(i) == S:
+#             counter += 1
+    
+#     return counter
+
+if __name__ == "__main__":
+    unittest.main()
+
+
 
 
