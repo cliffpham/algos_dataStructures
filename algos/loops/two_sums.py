c70ba821c1fd024c6a9fcd63af1c14788d32a031
cliffpham <cliffpham@gmail.com>
2019-06-01 00:56:55 -0700

    [sort/two_pointers] improved solution for: https://leetcode.com/problems/two-sum/ 88% | faster than other python solutions

--- algos/loops/two_sums.py
+++ algos/loops/two_sums.py
@@ -1,32 +1,36 @@
-# import unittest
+import unittest
 
-# class test_two_sum(unittest.TestCase):
-#     def test1_two_sum(self):
-#         self.assertEquals(
-#             two_sum([2,7,11,15], 9)
-#             [0,1]
-#         )
+# store the the values in the arr and the index in a new arr
+# sort the new arr so that you can use the two pointer method
+# with the two pointer method on a sorted arr with access to its index you can solve it one loop
 
-def two_sum(arr, target):
-    i = 0
-    j = len(arr) - 1
+class test_two_sum(unittest.TestCase):
+     def test1_two_sum(self):
+         self.assertEqual(
+             solve([2,7,11,15], 9),
+             [0,1]
+         )
+
+def solve(arr, target):
+    result = []
     ans = []
-
+    i = 0
+    j = len(arr)-1
+    
+    for n in range(len(arr)):
+        result.append((arr[n], n))
+    result.sort()
+    
     while i < j:
-        if arr[i] + arr[j] == target:
-            ans.append(i)
-            ans.append(j)
+        if result[i][0] + result[j][0] == target:
+            ans.append(result[i][1])
+            ans.append(result[j][1])
             break
-
-        if arr[i] + arr[j] > target:
+        if result[i][0] + result[j][0] > target:
             j -= 1
-
-        if arr[i] + arr[j] < target:
+        if result[i][0] + result[j][0] < target:
             i += 1
-
     return ans
 
-# print(two_sum([2,7,11,15], 9))
-
-# if __name__ == "__main__":
-#     unittest.main()
+if __name__ == "__main__":
+     unittest.main()
