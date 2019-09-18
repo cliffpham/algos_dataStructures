3169c8b31a45f65ba7d43577ebe495ac0b8b55e9
cliffpham <cliffpham@gmail.com>
2019-09-17 17:11:43 -0700

    solution for: https://leetcode.com/problems/longest-palindrome/

---
+++ algos/loops/longest_palindrome.py
@@ -0,0 +1,26 @@
+import unittest
+from collections import Counter
+
+class Test(unittest.TestCase):
+    def test1(self):
+        self.assertEquals(
+                solve("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"),
+                983
+        )
+
+def solve(s):
+    counter = Counter(s)
+    result = 0
+    single_letter = 0
+
+    for k,v in counter.items():
+        if v % 2 == 0:
+            result += v
+        else:
+            result += v - 1
+            single_letter = 1
+
+    return result + single_letter
+
+if __name__ == "__main__":
+    unittest.main()
